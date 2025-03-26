6bbe7d346ea70f4745d15d80b1e29064e0f0c435
Nicolas Bouillon <nicolas.bouillon.pro1@gmail.com>
2025-03-26 11:41:51 +0100

    ajout dans la db fonctionnel

--- src/Controller/Shop/ProductController.php
+++ src/Controller/Shop/ProductController.php
@@ -5,8 +5,11 @@ namespace App\Controller\Shop;
 use App\Entity\CartItem;
 use App\Entity\Cart;
 use App\Entity\Product;
+use App\Form\ProductType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -24,34 +27,19 @@ final class ProductController extends AbstractController
 
 
     #[Route('/list', name: '_list')]
-    public function listAction(): Response
+    public function listAction(EntityManagerInterface $em): Response
     {
-        /*
-        $entityManager = $this->getDoctrine()->getManager();
-        $products = $entityManager->getRepository(Product::class)->findAll();
 
-        $productData = [];
-        foreach ($products as $product) {
-            $productData[] = [
-                'id' => $product->getId(),
-                'name' => $product->getName(),
-                'price' => $product->getPrice(),
-            ];
-        }
-        */
-        $produits = [
-            ['libelle' => 'Produit 1', 'prix' => 10, 'stock' => 10],
-            ['libelle' => 'Produit 2', 'prix' => 20, 'stock' => 20],
-        ];
+        $products = $em->getRepository(Product::class)->findAll();
 
-        return $this->render("shop/product/list.html.twig", ['produits' => $produits]);
+        return $this->render("shop/product/list.html.twig", ['produits' => $products]);
     }
 
     #[Route('/panier', name: '_panier')]
     public function panierAction(EntityManagerInterface $em): Response
     {
 
-        $products = $em->getRepository(Product::class)->findAll();
+        $products = $em->getRepository(Cart::class)->findAll();
 
 
         $items = [
@@ -63,31 +51,30 @@ final class ProductController extends AbstractController
     }
 
     #[Route('/add/product', name: '_add_product', methods: ['GET', 'POST'])]
-    public function addProductAction(EntityManagerInterface $em): Response
+    public function addProductAction(EntityManagerInterface $em, Request $request): Response
     {
-        // Vérifier si le formulaire a été soumis
-        if ($request->isMethod('POST')) {
-            // Récupérer les données du formulaire
-            $name = $request->request->get('productName');
-            $price = $request->request->get('productPrice');
-            $description = $request->request->get('productDescription');
+        $product = new Product();
+        $form = $this->createForm(ProductType::class, $product);
+        $form->add('submit', SubmitType::class);
+        $form->handleRequest($request);
 
-            // Créer une nouvelle entité Product et assigner les valeurs
-            $product = new Product();
-            $product->setName($name);
-            $product->setPrice($price);
-            $product->setDescription($description);
-
-            // Persister l'entité en base de données
+        if ($form->isSubmitted() && $form->isValid()) {
             $em->persist($product);
             $em->flush();
-
-            // Rediriger l'utilisateur, par exemple vers la liste des produits
+            $this->addFlash('info', 'ajout réussie');
             return $this->redirectToRoute('shop_product_list');
         }
 
+        if ($form->isSubmitted()){
+            $this->addFlash('info', 'formulaire incorrect');
+        }
+
+        $args = array(
+            'form' => $form,
+        );
+
         // Afficher le formulaire
-        return $this->render('shop/product/add_produit.html.twig');
+        return $this->render('shop/product/add_produit.html.twig', $args);
     }
 
 }
