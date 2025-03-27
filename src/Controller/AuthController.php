e157bee6811eb5774888b3982e1bf653ab54bdd9
Maxtrxn <pcmaxtron@gmail.com>
2025-03-27 15:59:04 +0100

    le formulaire de creation de commpte qui marche

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -2,9 +2,15 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use App\Form\UserType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 #[Route('/auth', name: 'auth')]
 final class AuthController extends AbstractController
 {
@@ -16,11 +22,29 @@ final class AuthController extends AbstractController
         ]);
     }
 
-    #[Route('/new', name: '_new')]
-    public function newAction(): Response
+    #[Route('/new', name: 'auth_new')]
+    public function register(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
     {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Hasher le mot de passe
+            $hashedPassword = $passwordHasher->hashPassword($user, $user->getPassword());
+            $user->setPassword($hashedPassword);
+
+            $user->setRoles(['ROLE_USER']);
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirect('/auth');
+        }
+
         return $this->render('auth/new.html.twig', [
-            'controller_name' => 'AuthController',
+            'form' => $form->createView(),
         ]);
     }
 }
