df887f4050e2a1f82961ab7aa0409ebfb2025b34
Maxtrxn <pcmaxtron@gmail.com>
2025-03-28 18:59:43 +0100

    en cours de parametrage pour la connexion

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -3,14 +3,15 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\UserType;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-use App\Form\UserType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
 #[Route('/auth', name: 'auth')]
 final class AuthController extends AbstractController
 {
@@ -46,5 +47,26 @@ final class AuthController extends AbstractController
         return $this->render('auth/new.html.twig', [
             'form' => $form->createView(),
         ]);
+    }
+
+    #[Route(path: '/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    #[Route(path: '/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -3,11 +3,11 @@
 namespace App\Form;
 
 use App\Entity\User;
-use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 
 class UserType extends AbstractType
@@ -15,11 +15,21 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username')
-            ->add('password', PasswordType::class)
-            ->add('name')
-            ->add('surname')
-            ->add('country');
+            ->add('username', TextType::class, [
+                'label' => 'Nom d’utilisateur'
+            ])
+            ->add('name', TextType::class, [
+                'label' => 'Prénom'
+            ])
+            ->add('surname', TextType::class, [
+                'label' => 'Nom de famille'
+            ])
+            ->add('country', TextType::class, [
+                'label' => 'Pays'
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'Mot de passe'
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
